c9314fdbb961ae14c19e2adac7224c998e264a22
mpvermette <marie-pier.vermette@hotmail.com>
2023-05-18 21:44:31 -0400

    Card et image

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-
+import Card from "./component/card/Card";
 
 function App() {
   return (
     <section className="app">
-    <>
-    
-       
-    </>
+      <Card
+      mainImageSrc="https://placehold.co/1000x400"
+      mainImageAlt="Some Image"
+      mainImageoading="lazy"
+      mainImageWidth="1000"
+      mainImageHeight="400"
+      />
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
