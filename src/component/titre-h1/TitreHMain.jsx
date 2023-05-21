36676f1cb27f5a3f27140d1622bd910633786a23
mpvermette <marie-pier.vermette@hotmail.com>
2023-05-21 12:31:50 -0400

    deux colonnes avec un titre

---
+++ src/component/titre-h1/TitreHMain.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./TitreHMain.css";
+
+const TitreHMain = (props) => {
+    return <h1 className='titrehmain'>{props.titreHMain}</h1>;
+};
+
+export default TitreHMain;
